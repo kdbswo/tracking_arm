769a60be540ed1bf62ff5c38be54ae869524c45e
kyj <kdbswo@gmail.com>
2025-11-05 20:50:18 +0900

    node setup에 등록

--- ws/src/tracking_control_py/setup.py
+++ ws/src/tracking_control_py/setup.py
@@ -1,26 +1,26 @@
 from setuptools import find_packages, setup
 
-package_name = 'tracking_control_py'
+package_name = "tracking_control_py"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools', 'pymycobot'],
+    install_requires=["setuptools", "pymycobot"],
     zip_safe=True,
-    maintainer='addinedu',
-    maintainer_email='kdbswo@gmail.com',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="addinedu",
+    maintainer_email="kdbswo@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'motion_test_node = tracking_control_py.motion_test_node:main',
+        "console_scripts": [
+            "motion_test_node = tracking_control_py.motion_test_node:main",
+            "arm_follower_node = tracking_control_py.arm_follower_node:main",
         ],
     },
 )
